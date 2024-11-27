a4434104a8ebc8be4dbd9f9d8d1687f308abc221
Lucianogrc <a01645870@tec.mx>
2024-11-26 18:55:55 -0600

    Update RecommendationSystem.h

--- RecommendationSystem.h
+++ RecommendationSystem.h
@@ -1,22 +1,26 @@
-#ifndef RECOMMENDATIONSYSTEM_H
-#define RECOMMENDATIONSYSTEM_H
+#ifndef RECOMMENDATION_SYSTEM_H
+#define RECOMMENDATION_SYSTEM_H
 
-#include "Graph.h"
 #include "UserManager.h"
 #include "ContentManager.h"
+#include "Graph.h"
+
+#include <set>
+#include <string>
+#include <vector>
 
 class RecommendationSystem {
-private:
-    Graph graph;
-    UserManager userManager;
-    ContentManager contentManager;
-
 public:
     void addUser(const std::string& username);
-    void addInterest(const std::string& username, const std::string& interest);
     void addContent(const std::string& category, const std::string& content);
     void addFriend(const std::string& user1, const std::string& user2);
+    void addInterest(const std::string& username, const std::string& category);
     std::vector<std::string> recommendContent(const std::string& username);
+
+private:
+    UserManager userManager;
+    ContentManager contentManager;
+    Graph graph;
 };
 
 #endif
