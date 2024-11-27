84523c4c4ef871b78992c7680e1f63227cb82f79
Lucianogrc <a01645870@tec.mx>
2024-11-26 18:34:38 -0600

    Update RecommendationSystem.cpp

--- RecommendationSystem.cpp
+++ RecommendationSystem.cpp
@@ -1,52 +1,92 @@
-#ifndef RECOMMENDATIONSYSTEM_H
-#define RECOMMENDATIONSYSTEM_H
+#include "RecommendationSystem.h"
 
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <queue>
-#include <string>
+// Graph Methods
+void Graph::addEdge(const std::string& user1, const std::string& user2) {
+    adjList[user1].push_back(user2);
+    adjList[user2].push_back(user1);
+}
 
-class Graph {
-private:
-    std::unordered_map<std::string, std::vector<std::string>> adjList;
+std::unordered_set<std::string> Graph::bfs(const std::string& startUser) {
+    std::unordered_set<std::string> visited;
+    std::queue<std::string> toVisit;
 
-public:
-    void addEdge(const std::string& user1, const std::string& user2);
-    std::unordered_set<std::string> bfs(const std::string& startUser);
-};
+    toVisit.push(startUser);
+    visited.insert(startUser);
 
-class ContentManager {
-private:
-    std::unordered_map<std::string, std::vector<std::string>> contentByInterest;
+    while (!toVisit.empty()) {
+        std::string currentUser = toVisit.front();
+        toVisit.pop();
 
-public:
-    void addContent(const std::string& interest, const std::string& content);
-    std::vector<std::string> getContentByInterest(const std::string& interest);
-};
+        for (const auto& neighbor : adjList[currentUser]) {
+            if (visited.find(neighbor) == visited.end()) {
+                visited.insert(neighbor);
+                toVisit.push(neighbor);
+            }
+        }
+    }
 
-class UserManager {
-private:
-    std::unordered_map<std::string, std::vector<std::string>> userInterests;
+    return visited;
+}
 
-public:
-    void addUser(const std::string& user);
-    void addInterest(const std::string& user, const std::string& interest);
-    std::vector<std::string> getInterests(const std::string& user);
-};
+// ContentManager Methods
+void ContentManager::addContent(const std::string& interest, const std::string& content) {
+    contentByInterest[interest].push_back(content);
+}
 
-class RecommendationSystem {
-private:
-    Graph graph;
-    ContentManager contentManager;
-    UserManager userManager;
+std::vector<std::string> ContentManager::getContentByInterest(const std::string& interest) {
+    return contentByInterest[interest];
+}
 
-public:
-    void addUser(const std::string& user);
-    void addContent(const std::string& interest, const std::string& content);
-    void addFriend(const std::string& user1, const std::string& user2);
-    std::vector<std::string> recommendContent(const std::string& user);
-};
+// UserManager Methods
+void UserManager::addUser(const std::string& user) {
+    if (userInterests.find(user) == userInterests.end()) {
+        userInterests[user] = {};
+    }
+}
 
-#endif // RECOMMENDATIONSYSTEM_H
+void UserManager::addInterest(const std::string& user, const std::string& interest) {
+    userInterests[user].push_back(interest);
+}
+
+std::vector<std::string> UserManager::getInterests(const std::string& user) {
+    return userInterests[user];
+}
+
+// RecommendationSystem Methods
+void RecommendationSystem::addUser(const std::string& user) {
+    userManager.addUser(user);
+}
+
+void RecommendationSystem::addContent(const std::string& interest, const std::string& content) {
+    contentManager.addContent(interest, content);
+}
+
+void RecommendationSystem::addFriend(const std::string& user1, const std::string& user2) {
+    graph.addEdge(user1, user2);
+}
+
+std::vector<std::string> RecommendationSystem::recommendContent(const std::string& user) {
+    std::unordered_set<std::string> friends = graph.bfs(user);
+    std::unordered_map<std::string, int> interestCount;
+    std::vector<std::string> recommendations;
+
+    // Obtener intereses de los amigos
+    for (const auto& friendUser : friends) {
+        if (friendUser != user) { // Excluir al usuario actual
+            std::vector<std::string> interests = userManager.getInterests(friendUser);
+            for (const auto& interest : interests) {
+                interestCount[interest]++;
+            }
+        }
+    }
+
+    // Recomendar contenido basado en intereses populares
+    for (const auto& [interest, count] : interestCount) {
+        std::vector<std::string> content = contentManager.getContentByInterest(interest);
+        recommendations.insert(recommendations.end(), content.begin(), content.end());
+    }
+
+    return recommendations;
+}
+
 
