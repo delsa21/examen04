e7134de5a9f38c237d38938f4655ddf5e33118e8
ChavaGG <A01639338@tec.mx>
2024-11-27 15:00:02 -0600

    Update userManager.h

--- userManager.h
+++ userManager.h
@@ -7,7 +7,7 @@
 
 class UserManager {
 public:
-    void añadirUsuario(const std::string& username);
+    void addUsuario(const std::string& username);
     void addInterest(const std::string& username, const std::string& category);
     std::set<std::string> getInterests(const std::string& username) const;
 
