d2541195c32bcf926c8872b3fc05223c703b6e54
Lucianogrc <a01645870@tec.mx>
2024-11-26 18:32:48 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,3 +5,27 @@ int main() {
     menu.showMenu();
     return 0;
 }
+#include <iostream>
+#include "Graph.h"
+//Graph
+int main() {
+    Graph graph;
+
+    // Agregar relaciones de amistad
+    graph.addEdge("Alice", "Bob");
+    graph.addEdge("Alice", "Charlie");
+    graph.addEdge("Bob", "David");
+    graph.addEdge("Charlie", "Eve");
+    graph.addEdge("Eve", "Frank");
+
+    // Realizar una búsqueda en amplitud desde Alice
+    std::unordered_set<std::string> friends = graph.bfs("Alice");
+
+    // Imprimir los amigos directos e indirectos de Alice
+    std::cout << "Amigos de Alice:" << std::endl;
+    for (const auto& friendName : friends) {
+        std::cout << friendName << std::endl;
+    }
+
+    return 0;
+}
